47e0cfbe9dc5fcfccd526c759b07d9319141e4d0
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-21 00:12:11 +0200

    cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,18 @@
 #include "RegularUnit.hpp"
 #include "Player.hpp"
 #include "test.hpp"
+#include "Game.hpp"
 
 int main() {
     Board board;
-    Player playerA("A", TURN::PLAYER_A);
-    Player playerB("B", TURN::PLAYER_B);
+    Player playerA("Red-A", TURN::PLAYER_A);
+    Player playerB("Blue-B", TURN::PLAYER_B);
+    GameView game_view(playerA, playerB);
+    GameController game_controller(playerA, playerB, game_view);
+    Game game(game_view, game_controller);
+    game.run();
+    
 
-    testing(board, playerA, playerB);
+    // testing(board, playerA, playerB);
     
 }
